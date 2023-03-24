4365f25ba27db1f17648be1163666c88477b54b1
gabriloren <gabriel.lg90@gmail.com>
2023-03-24 16:51:20 +0100

    Creación clase Types (Enum)

---
+++ src/pokemon/Types.java
@@ -0,0 +1,21 @@
+package pokemon;
+
+public enum Types {
+	
+	WATER,
+	BUG,
+	DRAGON,
+	ELCTRIC,
+	GHOST,
+	FIRE,
+	ICE,
+	FIGHTINH,
+	NORMAL,
+	GRASS,
+	PSYCHIC,
+	ROCK,
+	GROUND,
+	POISON,
+	FLYING,
+
+}
