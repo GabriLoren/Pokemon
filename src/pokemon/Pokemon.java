a54ce32bf692702282a4cc0c0214bd896056bfe4
gabriloren <gabriel.lg90@gmail.com>
2023-03-24 16:31:30 +0100

    Creación constructores clase Pokemon

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -18,6 +18,52 @@ public class Pokemon {
 	private boolean gender;
 	private Type type;
 	private String status;
-	private Obj obj;
+	private Obj objeto;
+	
+	public Pokemon() {
+		
+		this.id = 0;
+		this.name = "";
+		this.nickname = "";
+		this.vit = 0;
+		this.atk = 0;
+		this.def = 0;
+		this.spAtk = 0;
+		this.spDef = 0;
+		this.speed = 0;
+		this.stamina = 0;
+		this.level = 0;
+		this.moves = new ArrayList<Move>();
+		this.fertility = 0;
+		this.gender = false;
+		this.type = new Type();
+		this.status = "";
+		this.objeto = new Obj();
+		
+	public Pokemon(int id, String name, String nickname, int vit, int atk, int def, int spAtk, int spDef, int speed,
+			int stamina, int level, ArrayList<Move> moves, int fertility, boolean gender, Type type, String status,
+			Obj obj) {
+		
+		this.id = id;
+		this.name = name;
+		this.nickname = nickname;
+		this.vit = vit;
+		this.atk = atk;
+		this.def = def;
+		this.spAtk = spAtk;
+		this.spDef = spDef;
+		this.speed = speed;
+		this.stamina = stamina;
+		this.level = level;
+		this.moves = moves;
+		this.fertility = fertility;
+		this.gender = gender;
+		this.type = type;
+		this.status = status;
+		this.obj = obj;
+	}
+	
+	
+	
 
 }
