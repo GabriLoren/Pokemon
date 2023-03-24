d96e0a13e8f1734d50db862a7dfa4e673595eff4
gabriloren <gabriel.lg90@gmail.com>
2023-03-24 16:17:26 +0100

    Creación de atributos de la clase Pokemon

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -2,4 +2,23 @@ package pokemon;
 
 public class Pokemon {
 
+	private int id;
+	private String name;
+	private String nickname;
+	private int vit;
+	private int atk;
+	private int def;
+	private int spAtk;
+	private int spDef;
+	private int speed;
+	private int stamina;
+	private int level;
+	private ArrayList<Move> moves;
+	private int fertility;
+	private boolean gender;
+	private Type type;
+	private String status;
+	private Obj obj;
+	private int A;
+
 }
