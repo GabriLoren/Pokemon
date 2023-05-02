bd69d00ebbce28ed5aae5bcc542a83561b323832
José Antonio Alacid Pérez <joseantonio.alacidp@gmail.com>
2023-05-02 12:51:32 +0200

    Clases typechart

---
+++ src/modelo/EffectValues.java
@@ -0,0 +1,6 @@
+package modelo;
+
+//Contains the values for what type advantage will deal in extra damage 
+public enum EffectValues {
+	INEFFECTIVE, WEAK, NEUTRAL, ADVANTAGE;
+}
--- src/modelo/LinkedlistObjetosPrueba.java
+++
@@ -1,7 +0,0 @@
-package modelo;
-
-public class LinkedlistObjetosPrueba {
-	
-	Obj pesa=new Obj(1,"pesa",1.2,.2,0,-0.2,0);
-
-}
